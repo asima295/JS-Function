1d715a884563e7160753f324d699199964b8fcd0
asima295 <ftasleem581@gmail.com>
2024-10-29 09:28:06 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+document.getElementById("name").innerHTML="heading"
+document.getElementById("name").style.color="red";
+document.getElementById("name").style.fontSize="50px";
+document.getElementById("d1").innerHTML="z";
+document.getElementById("d1").innerHTML =
++ z + ".";
+<button onclick="window.print(z)">print</button>
+
+
